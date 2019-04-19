9a035688ce40a1c5e3886ad7a16db4df3fef2e65
alecthibodeau <alecthibodeau@gmail.com>
2019-04-19 11:21:33 -0400

    Add shopping cart to nav.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function () {
     console.log('document ready')
+    $('.cart-image').hover(function () {
+        $(this).attr('src','public/images/nav_view_cart_fill.svg')
+    }, function () {
+        $(this).attr('src','public/images/nav_view_cart_lines.svg')
+    })
 })
   
 console.log('app.js runs')
