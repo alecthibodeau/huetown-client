850ae7f94684ac543eb7a4d6f4c8b3dad6d2a271
alecthibodeau <alecthibodeau@gmail.com>
2019-04-20 06:45:09 -0400

    Add JS hover for icons.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,29 @@
 
 $(document).ready(function () {
     console.log('document ready')
+
     $('.cart-image').hover(function () {
         $(this).attr('src','public/images/nav_view_cart_fill.svg')
     }, function () {
         $(this).attr('src','public/images/nav_view_cart_lines.svg')
+    })
+
+    $('.instagram').hover(function () {
+        $('.icon-bg-instagram').css('visibility','visible')
+    }, function () {
+        $('.icon-bg-instagram').css('visibility','hidden')
+    })
+
+    $('.twitter').hover(function () {
+        $('.icon-bg-twitter').css('visibility','visible')
+    }, function () {
+        $('.icon-bg-twitter').css('visibility','hidden')
+    })
+
+    $('.email').hover(function () {
+        $('.icon-bg-email').css('visibility','visible')
+    }, function () {
+        $('.icon-bg-email').css('visibility','hidden')
     })
 })
   
