3683785e75af58fef884faea43ed84725bd4767d
alecthibodeau <alecthibodeau@gmail.com>
2019-04-23 05:33:31 -0400

    Main work experiments.

---
+++ assets/scripts/page-content.js
@@ -0,0 +1,9 @@
+const pageContent = {
+    "pages": {
+        "about": "<div class=\"about-content\">Hoy</div>"
+    }
+}
+
+module.exports = {
+    pageContent
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+'use strict'
+
+// require('./assets/scripts/app.js')
