e174a3a3a65970d544da67f81723a7563c140bd3
alecthibodeau <alecthibodeau@gmail.com>
2019-06-18 15:48:11 -0400

    Track browsing history and page title.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -35,8 +35,39 @@ $(() => {
   // Set variable for main element
   let mainDiv = document.getElementById('main');
 
+  // Display correct content when user navigates back in browsing history
+  window.onpopstate = () => {
+    mainDiv.innerHTML = routes[window.location.pathname];
+    console.log(`back route is ${window.location.pathname}`);
+  };
+
+  // Add current page’s url (url origin + url pathname) to user’s navigation history
+  let onNavElementClick = (pathName) => {
+    window.history.pushState({}, pathName, window.location.origin + pathName);
+    mainDiv.innerHTML = routes[pathName];
+    console.log(`pathName is ${pathName}`);
+  };
+
   // Change content of main element based on current route
-  mainDiv.innerHTML = routes[window.location.pathname];
+  $('.nav-element').click(function () {
+    event.preventDefault();
+    let currentPagePath = $(this).attr('href');
+    let currentPageTitle = $(this).attr('title');
+    console.log('%c NAV ELEMENT CLICKED! ', 'font-size: 20px; background: #00ff00; color: #000000');
+    console.log(`currentPagePath is ${currentPagePath}`);
+    // Update page title based on 
+    if (currentPageTitle === "Home") {
+      document.title = "Huetown"
+      } else {
+      document.title = currentPageTitle + " | Huetown"
+      }
+    console.log(`document.title is ${document.title}`);
+    onNavElementClick(currentPagePath);
+    }
+  );
+
+  // Change content of main element based on current route
+  // mainDiv.innerHTML = routes[window.location.pathname];
 });
   
 console.log('app.js runs');
