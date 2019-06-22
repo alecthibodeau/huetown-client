78f2bda3c9f09b53ecc6925454afc5faaa99d1ef
alecthibodeau <alecthibodeau@gmail.com>
2019-06-21 20:23:47 -0400

    Add start of postcards page.

---
+++ assets/scripts/pages/postcards.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let postcardsPage = `
+  <div class="postcards-page">
+    <div class="row">
+      <div class="col">
+        <a href="/assets">
+          <img src="public/images/items/postcard_assets.png" />
+        </a>
+        <p>Assets</p>
+      </div>
+      <div class="col">
+        <a href="/assets">
+          <img src="public/images/items/postcard_assets.png" />
+        </a>
+        <p>Assets</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <a href="/assets">
+          <img src="public/images/items/postcard_assets.png" />
+        </a>
+        <p>Assets</p>
+      </div>
+      <div class="col">
+        <a href="/assets">
+          <img src="public/images/items/postcard_assets.png" />
+        </a>
+        <p>Assets</p>
+      </div>
+    </div>
+  </div>
+`;
+
+export default {
+  postcardsPage
+}
--- assets/scripts/routes.js
+++ assets/scripts/routes.js
@@ -1,18 +1,19 @@
 import about from './pages/about.js';
+import postcards from './pages/postcards.js';
 
 const homePage = "homePage";
 const lunarCalendarsPage = "lunarCalendarsPage";
-const postcardsPage = "postcardsPage";
+// const postcardsPage = "postcardsPage";
 const printsPage = "printsPage";
 
 const routes = {
     '/': homePage,
     '/lunar-calendars': lunarCalendarsPage,
-    '/postcards': postcardsPage,
+    '/postcards': postcards.postcardsPage,
     '/prints': printsPage,
-    '/about': about.aboutPage,
+    '/about': about.aboutPage
   };
   
   export default {
     routes
-  }
+  };
