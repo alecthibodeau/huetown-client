187b24640fcc16e566c92a0114706937661c5480
alecthibodeau <alecthibodeau@gmail.com>
2019-06-28 07:44:20 -0400

    Modify setHtml function.

--- assets/scripts/routes.js
+++ assets/scripts/routes.js
@@ -7,14 +7,11 @@ import prints from './pages/prints.js';
 import postcards from './pages/postcards.js';
 import about from './pages/about.js';
 
-// item pages
+// item page
 import item from './pages/items/item-page.js';
 
-// const homePage = "homePage";
-// const lunarCalendarsPage = "lunarCalendarsPage";
-
 const routesDictionary = {
-    '/huetown-client/': home.homePage,
+    '/': home.homePage,
     '/lunar-calendars': lunarCalendars.lunarCalendarsPage,
     '/postcards': postcards.postcardsPage,
     '/prints': prints.printsPage,
