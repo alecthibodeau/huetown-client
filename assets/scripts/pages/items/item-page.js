6c2c20e44f5d9140e6a9567dc61414ec0a6a86cb
alecthibodeau <alecthibodeau@gmail.com>
2019-06-30 09:58:09 -0400

    Add items list. Start modular items page.

--- assets/scripts/pages/items/item-page.js
+++ assets/scripts/pages/items/item-page.js
@@ -1,28 +1,29 @@
 'use strict'
 
+import items from './items-list.js';
+
 let itemPage = `
   <div class="item-page">
     <div class="row">
 
       <div class="col">
-        <a href="/">
-          <img src="public/images/items/postcard_assets.png" />
-        </a>
+        <img src="public/images/items/postcard_assets.png" />
       </div>
 
       <div class="col">
         <p>
-          Assets postcard
+          <span class="item-name"></span> 
+          <span class="item-category">postcard</span>
         </p>
-        <p>
+        <p class="item-info-one">
           6 x 4 inches; mellow, satin finish
         </p>
-        <p>
+        <p class="item-info-two">
           Full-color front / single-color back 
         </p>
-        <p>
+        <p class="item-info-three">
           Caption on the back: “Assets (detail) by Alec Thibodeau, 2012, screen print on paper, 24 x 18 inches”
-        <p>
+        <p class="item-price">
           $1 
         </p>
         <p>
@@ -30,7 +31,7 @@ let itemPage = `
         </p>
         <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
           <input type="hidden" name="cmd" value="_s-xclick">
-          <input type="hidden" name="hosted_button_id" value="4CL74PH4FSSQ6">
+          <input type="hidden" name="hosted_button_id" value="4CL74PH4FSSQ6" class="item-id">
           <input type="text" name="quantity" value="1">
           <input type="submit" value="add to cart" alt="Add To Cart">
           <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
@@ -41,6 +42,27 @@ let itemPage = `
   </div>
 `;
 
+let itemIndex = null;
+
+const findIndex = function (array, key, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][key] === value) {
+      return itemIndex = i;
+    }
+    return -1;
+  }
+}
+
+const itemLoad =  function (currentRoute) {
+  findIndex(items.itemsList, 'itemRoute', currentRoute);
+  if (itemIndex !== null && itemIndex !== -1) {
+    console.log(`Cachalot cart is ${items.itemsList[1].itemId}`)
+    console.log(`Current cart is ${items.itemsList[itemIndex].itemId}`)
+    document.getElementsByClassName('item-name')[0].innerHTML = items.itemsList[itemIndex].itemName
+  }
+}
+
 export default {
-  itemPage
+  itemPage,
+  itemLoad
 };
--- assets/scripts/pages/postcards.js
+++ assets/scripts/pages/postcards.js
@@ -5,7 +5,7 @@ let postcardsPage = `
     <div class="row">
       <div class="col">
         <a data-title="Assets" class="item-element" href="/assets">
-          <img src="/huetown-client/public/images/items/postcard_assets.png" />
+          <img src="/public/images/items/postcard_assets.png" />
         </a>
         <p>Assets</p>
       </div>
