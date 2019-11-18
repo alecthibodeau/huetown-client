901fe559649557b3ed168617496b4e565b136cc3
alecthibodeau <alecthibodeau@gmail.com>
2019-11-18 08:17:25 -0500

    Add additional info for items.

--- assets/scripts/pages.js
+++ assets/scripts/pages.js
@@ -10,10 +10,16 @@ import about from './pages/about.js';
 // item page
 import item from './pages/items/item-page.js';
 
-const postcardUniversals = {
+const itemInfoUniversals = {
+  lunarCalendarPrice: 40,
+  lunarCalendarInfoOne: '19 x 11.75 inches<br>Letterpress print',
+  lunarCalendarInfoTwo: 'Printed by DWRI Letterpress',
+  lunarCalendarInfoThree: 'Hand numbered and signed by the artist in a limited edition of prints',
+  lunarCalendarInfoFour: 'Ships rolled and triple-packaged: archival wrapper, inner protective tube and outer shipping container',
   postcardInfoOne: '6 x 4 inches; mellow, satin finish',
   postcardInfoTwo: 'Full-color front / single-color back',
-  postcardPrice: 1
+  postcardPrice: 1,
+  printPrice: 15
 }
 
 const pagesDictionary = {
@@ -32,15 +38,63 @@ const pagesDictionary = {
   'about': {
     mainContent: about.aboutPage
   },
+  'lunarCalendarTwenty': {
+    mainContent: item.itemPage,
+    itemName: 'The Instruments Agree',
+    itemCategory: '2020 lunar calendar by Alec Thibodeau',
+    itemImageFront: 'thibodeau_lunar_calendar_2020_photo_01.jpg',
+    itemInfoOne: itemInfoUniversals.lunarCalendarInfoOne,
+    itemInfoTwo: itemInfoUniversals.lunarCalendarInfoTwo,
+    itemInfoThree: 'Hand numbered and signed by the artist in a limited edition of 500 prints',
+    itemInfoFour: itemInfoUniversals.lunarCalendarInfoFour,
+    itemPrice: itemInfoUniversals.lunarCalendarPrice,
+    itemId: 'TGVEUGQD58AX2'
+  },
+  'lunarCalendarNineteen': {
+    mainContent: item.itemPage,
+    itemName: 'Orchard Quest',
+    itemCategory: '2019 lunar calendar by Alec Thibodeau',
+    itemImageFront: 'thibodeau_lunar_calendar_2019_photo_01.jpg',
+    itemInfoOne: itemInfoUniversals.lunarCalendarInfoOne,
+    itemInfoTwo: itemInfoUniversals.lunarCalendarInfoTwo,
+    itemInfoThree: 'Hand numbered and signed by the artist in a limited edition of 500 prints',
+    itemInfoFour: itemInfoUniversals.lunarCalendarInfoFour,
+    itemPrice: itemInfoUniversals.lunarCalendarPrice,
+    itemId: 'T45GH9BL83VQ4'
+  },
+  'lunarCalendarEighteen': {
+    mainContent: item.itemPage,
+    itemName: 'Shimmery Circuit',
+    itemCategory: '2018 lunar calendar by Alec Thibodeau',
+    itemImageFront: 'thibodeau_lunar_calendar_2018_photo_01.jpg',
+    itemInfoOne: itemInfoUniversals.lunarCalendarInfoOne,
+    itemInfoTwo: itemInfoUniversals.lunarCalendarInfoTwo,
+    itemInfoThree: 'Hand numbered and signed by the artist in a limited edition of 1000 prints',
+    itemInfoFour: itemInfoUniversals.lunarCalendarInfoFour,
+    itemPrice: itemInfoUniversals.lunarCalendarPrice,
+    itemId: '4LN49ZLYQLY88'
+  },
+  'lunarCalendarSeventeen': {
+    mainContent: item.itemPage,
+    itemName: 'I Still See the X',
+    itemCategory: '2017 lunar calendar by Alec Thibodeau',
+    itemImageFront: 'thibodeau_lunar_calendar_2017_photo_01.jpg',
+    itemInfoOne: itemInfoUniversals.lunarCalendarInfoOne,
+    itemInfoTwo: itemInfoUniversals.lunarCalendarInfoTwo,
+    itemInfoThree: 'Hand numbered and signed by the artist in a limited edition of 1000 prints',
+    itemInfoFour: itemInfoUniversals.lunarCalendarInfoFour,
+    itemPrice: itemInfoUniversals.lunarCalendarPrice,
+    itemId: 'C4RW9LGDQ8NYL'
+  },
   'postcardAssets': {
     mainContent: item.itemPage,
     itemName: 'Assets',
     itemCategory: 'postcard',
     itemImageFront: 'public/images/items/postcard_assets.png',
-    itemInfoOne: postcardUniversals.postcardInfoOne,
-    itemInfoTwo: postcardUniversals.postcardInfoTwo,
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
     itemInfoThree: `Caption on the back: "<span class="ital">Assets</span> (detail) by Alec Thibodeau, 2012, screen print on paper, 24 x 18 inches"`,
-    itemPrice: postcardUniversals.postcardPrice,
+    itemPrice: itemInfoUniversals.postcardPrice,
     itemId: '4CL74PH4FSSQ6'
   },
   'postcardCachalot': {
@@ -48,10 +102,10 @@ const pagesDictionary = {
     itemName: 'Cachalot',
     itemCategory: 'postcard',
     itemImageFront: 'public/images/items/postcard_cachalot.png',
-    itemInfoOne: postcardUniversals.postcardInfoOne,
-    itemInfoTwo: postcardUniversals.postcardInfoTwo,
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
     itemInfoThree: `Caption on the back: "The cachalot, or sperm whale, swims in the open ocean and has the largest brain of any animal."`,
-    itemPrice: postcardUniversals.postcardPrice,
+    itemPrice: itemInfoUniversals.postcardPrice,
     itemId: '7H7ZNZJMAM5S4'
   },
   'postcardFlanneryOConnor': {
@@ -59,22 +113,79 @@ const pagesDictionary = {
     itemName: `Flannery O'Connor`,
     itemCategory: 'postcard',
     itemImageFront: 'public/images/items/postcard_flannery_oconnor.png',
-    itemInfoOne: postcardUniversals.postcardInfoOne,
-    itemInfoTwo: postcardUniversals.postcardInfoTwo,
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
     itemInfoThree: `Caption on the back: "Flannery O’Connor (1925–1964) wrote <span class="ital">Wise Blood</span> and was an accomplished cartoonist."`,
-    itemPrice: postcardUniversals.postcardPrice,
+    itemPrice: itemInfoUniversals.postcardPrice,
     itemId: '25LJT9358P2PS'
+  },
+  'postcardGreatWhiteShark': {
+    mainContent: item.itemPage,
+    itemName: 'Great White Shark',
+    itemCategory: 'postcard',
+    itemImageFront: 'public/images/items/postcard_great_white_shark.png',
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
+    itemInfoThree: `Caption on the back: "The great white shark inhabits most of the world's oceans and wants to be your friend."`,
+    itemPrice: itemInfoUniversals.postcardPrice,
+    itemId: 'UP6L2CNKJLSL2'
   },
   'postcardHermanMelville': {
     mainContent: item.itemPage,
     itemName: 'Herman Melville',
     itemCategory: 'postcard',
     itemImageFront: 'public/images/items/postcard_herman_melville.png',
-    itemInfoOne: postcardUniversals.postcardInfoOne,
-    itemInfoTwo: postcardUniversals.postcardInfoTwo,
-    itemInfoThree: `Caption on the back: "Herman Melville (1819–1891) wrote ‘Bartleby, the Scrivener’ and was a respected customs inspector."`,
-    itemPrice: postcardUniversals.postcardPrice,
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
+    itemInfoThree: `Caption on the back: "Herman Melville (1819–1891) wrote 'Bartleby, the Scrivener' and was a respected customs inspector."`,
+    itemPrice: itemInfoUniversals.postcardPrice,
     itemId: 'JL4EHBW8MVKCC'
+  },
+  'postcardHippopotamus': {
+    mainContent: item.itemPage,
+    itemName: 'Hippopotamus',
+    itemCategory: 'postcard',
+    itemImageFront: 'public/images/items/postcard_hippopotamus.png',
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
+    itemInfoThree: `Caption on the back: "The hippopotamus has a name which means 'horse of the river' in Ancient Greek."`,
+    itemPrice: itemInfoUniversals.postcardPrice,
+    itemId: 'T5XS8HXHARZG4'
+  },
+  'postcardRhodeIslandWhale': {
+    mainContent: item.itemPage,
+    itemName: 'Rhode Island Whale',
+    itemCategory: 'postcard',
+    itemImageFront: 'public/images/items/postcard_rhode_island_whale.png',
+    itemInfoOne: itemInfoUniversals.postcardInfoOne,
+    itemInfoTwo: itemInfoUniversals.postcardInfoTwo,
+    itemInfoThree: `Caption on the back: "The North Atlantic humpback whale swims in waters off Rhode Island and can grow to 50 feet in length."`,
+    itemPrice: itemInfoUniversals.postcardPrice,
+    itemId: 'WZXP99EBYSU5A'
+  },
+  'printFunktionslust': {
+    mainContent: item.itemPage,
+    itemName: 'Funktionslust for All of Us',
+    itemCategory: 'by Alec Thibodeau',
+    itemImageFront: 'public/images/items/print_funktionslust.png',
+    itemInfoOne: '5.75 x 5.75 inches<br>Screen print',
+    itemInfoTwo: 'Printed by the artist',
+    itemInfoThree: 'From a limited edition of 50 prints<br>Hand numbered and signed by the artist',
+    itemInfoFour: 'Unframed',
+    itemPrice: itemInfoUniversals.printPrice,
+    itemId: 'VARZ4F5RHAP9Y'
+  },
+  'printStentorian': {
+    mainContent: item.itemPage,
+    itemName: 'Stentorian',
+    itemCategory: 'by Alec Thibodeau',
+    itemImageFront: 'public/images/items/print_stentorian.png',
+    itemInfoOne: '5.5 x 5.5 inches<br>Letterpress print',
+    itemInfoTwo: 'Printed by DWRI Letterpress',
+    itemInfoThree: 'From a limited edition of 250 prints<br>Hand numbered and signed by the artist',
+    itemInfoFour: 'Unframed',
+    itemPrice: itemInfoUniversals.printPrice,
+    itemId: 'AUN3U4NQ52J2W'
   }
 };
 
--- assets/scripts/pages/items/item-page.js
+++ assets/scripts/pages/items/item-page.js
@@ -4,7 +4,8 @@ import pages from '../../pages.js';
 
 let itemPage = `
   <div class="item-page">
-    <div class="content-container">
+
+    <div class="content-container container-one">
 
       <div class="content-block">
         <img class="item-image-front" src="" />
@@ -20,6 +21,7 @@ let itemPage = `
           <div class="item-info-one"></div>
           <div class="item-info-two"></div>
           <div class="item-info-three"></div>
+          <div class="item-info-four"></div>
         </div>
 
         <div class="payment-info">
@@ -42,6 +44,10 @@ let itemPage = `
         
       </div>
     </div>
+
+    <div class="container-two">
+    </div>
+
   </div>
 `;
 
@@ -53,16 +59,19 @@ const itemLoad = function (currentPage) {
     ['.item-info-one', page.itemInfoOne],
     ['.item-info-two', page.itemInfoTwo],
     ['.item-info-three', page.itemInfoThree],
+    ['.item-info-four', page.itemInfoFour],
     ['.item-price', page.itemPrice]
   ]
   if (currentPage) {
-    // Use '.innerHTML' to fill each item's DOM element by class name
+    // Use '.innerHTML' to fill each item's DOM element by class name with the item's info -- if that info exists
     htmlToFill.forEach(function (element) {
-      document.querySelector(element[0]).innerHTML = element[1];
+      if (element[1]) {
+        document.querySelector(element[0]).innerHTML = element[1];
+      }
     });
-    // Use '.src' to set each image's source
+    // Use '.src' to set the main image's source
     document.querySelector('.item-image-front').src = page.itemImageFront;
-    // Use '.value' to set each input's value
+    // Use '.value' to set the form input's cart value
     document.querySelector('.item-id').value = page.itemId;
   }
 }
