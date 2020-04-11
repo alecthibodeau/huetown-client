fe43fca3541e4787ab4ae09d367423f151b4b98f
alecthibodeau <alecthibodeau@gmail.com>
2020-04-11 08:49:43 -0400

    Update Unbound info.

--- assets/scripts/pages/home.js
+++ assets/scripts/pages/home.js
@@ -4,27 +4,35 @@ let homePage = `
   <div class="home-page">
 
     <div class="unbound">
+
       <div class="unbound-top">
         <img src="public/images/home/unbound_logo.png" />
         <div class="unbound-announcement">
           Today April 11th, 2020 I'm participating in 
           <a class="text-link" href="https://unbound.risd.edu" target="_blank">RISD Unbound</a>.
-          As part of this celebration of Rhode Island artists, I'll be donating 50% of the proceeds from all online sales today to the 
+          Coinciding with this celebration of local artists, I'll be donating 50% of the proceeds from every sale today at Huetown to the 
           newly established <a class="text-link" href="https://www.artistcommunities.org/arf" target="_blank">Rhode Island Artist Relief Fund</a>.
-          And all orders placed today will include a free copy of my <span class="bold ital">Fold'em Funnies #1</span> minicomic, 
-          currently in the collection of the <a class="text-link" href="https://library.risd.edu" target="_blank">Fleet Library</a> at RISD.
+          And all orders placed today will include a free copy of my <span class="bold ital">Fold'em Funnies #1</span> minicomic 
+          (which is currently in the collection of the <a class="text-link" href="https://library.risd.edu" target="_blank">Fleet Library</a> at RISD). I'll be adding more pieces throughout the day. Stay tuned!
         </div>
       </div>
-      <div class="unbound-bottom">
-        RISD Unbound is a day-long event celebrating artists’ books, zines, 
-        and experimental printed matter created by RISD students and community, 
-        local artists and designers, as well as publishers, artists, designers, 
-        and enthusiasts from across the region. Through exhibits and sales, 
-        Unbound seeks to inspire conversations around cultural publishing 
-        in the Providence community.
-      </div>
-    </div>
 
+      <div class="unbound-bottom">
+        <div class="unbound-description">
+          RISD Unbound is a day-long event celebrating artists’ books, zines, 
+          and experimental printed matter created by RISD students and community, 
+          local artists and designers, as well as publishers, artists, designers, 
+          and enthusiasts from across the region. Through exhibits and sales, 
+          Unbound seeks to inspire conversations around cultural publishing 
+          in the Providence community.
+        </div>
+        <div>
+          <img src="public/images/home/ff001_2020_square.png" />
+        </div>
+      </div>
+
+    </div>
+  
   </div>
 `;
 
