dea4bfa4bfb4172a719deba1e7420ad6474790b9
alecthibodeau <alecthibodeau@gmail.com>
2020-04-11 08:53:27 -0400

    Update Unbound text.

--- assets/scripts/pages/home.js
+++ assets/scripts/pages/home.js
@@ -13,7 +13,7 @@ let homePage = `
           Coinciding with this celebration of local artists, I'll be donating 50% of the proceeds from every sale today at Huetown to the 
           newly established <a class="text-link" href="https://www.artistcommunities.org/arf" target="_blank">Rhode Island Artist Relief Fund</a>.
           And all orders placed today will include a free copy of my <span class="bold ital">Fold'em Funnies #1</span> minicomic 
-          (which is currently in the collection of the <a class="text-link" href="https://library.risd.edu" target="_blank">Fleet Library</a> at RISD). I'll be adding more pieces throughout the day. Stay tuned!
+          (currently in the collection of the <a class="text-link" href="https://library.risd.edu" target="_blank">Fleet Library</a> at RISD). I'll be adding more pieces throughout the day. Stay tuned!
         </div>
       </div>
 
