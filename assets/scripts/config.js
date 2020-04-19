40d56d77d4da4ff2edd59f10673a83aaf5ba56a8
alecthibodeau <alecthibodeau@gmail.com>
2020-04-18 22:38:01 -0400

    Complete adding six drawings.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -16,7 +16,7 @@ const itemInfoUniversals = {
   drawingInfoPenAndInk: 'pen and ink on Bristol board',
   drawingInfoThreeAndQuarterInches: '3.25 x 3.25 inches',
   drawingInfoSignedOnBack: 'signed on the back',
-  drawingPrice: 75,
+  drawingPrice: 55,
   lunarCalendarPrice: 40,
   lunarCalendarInfoOne: '19 x 11.75 inches<br>Letterpress print<br>Printed by DWRI Letterpress',
   lunarCalendarInfoTwo: '',
@@ -81,31 +81,83 @@ const pagesInfo = {
   'veganly': {
     mainContent: veganly.veganlyPage
   },
-  'drawingButternutBear': {
-    itemClass: 'drawing-butternut-bear',
+  'drawingBisonBreathes': {
+    itemClass: 'drawing-bison-breathes',
     itemCategory: 'drawing',
-    itemName: 'Butternut Bear',
+    itemName: 'Bison Breathes',
     itemSubname: '',
-    itemImageFront: 'public/images/home/drawings/drawing_butternut_bear_800px.png',
+    itemImageFront: 'public/images/home/drawings/drawing_bison_breathes_gs_500px.png',
     itemInfoOne: itemInfoUniversals.drawingInfoOriginalDrawing,
     itemInfoTwo: itemInfoUniversals.drawingInfoPenAndInk,
     itemInfoThree: itemInfoUniversals.drawingInfoThreeAndQuarterInches,
     itemInfoFour: itemInfoUniversals.drawingInfoSignedOnBack,
     itemPrice: itemInfoUniversals.drawingPrice,
-    itemId: ''
+    itemId: 'P32KQAB5THSDE'
+  },
+  'drawingButternutBear': {
+    itemClass: 'drawing-butternut-bear',
+    itemCategory: 'drawing',
+    itemName: 'Butternut Bear',
+    itemSubname: '',
+    itemImageFront: 'public/images/home/drawings/drawing_butternut_bear_gs_500px.png',
+    itemInfoOne: itemInfoUniversals.drawingInfoOriginalDrawing,
+    itemInfoTwo: itemInfoUniversals.drawingInfoPenAndInk,
+    itemInfoThree: itemInfoUniversals.drawingInfoThreeAndQuarterInches,
+    itemInfoFour: itemInfoUniversals.drawingInfoSignedOnBack,
+    itemPrice: itemInfoUniversals.drawingPrice,
+    itemId: 'ZPDMSTPEF8498'
   },
   'drawingHawkHeralds': {
     itemClass: 'drawing-hawk-heralds',
     itemCategory: 'drawing',
     itemName: 'Hawk Heralds',
     itemSubname: '',
-    itemImageFront: 'public/images/home/drawings/drawing_hawk_heralds_800px.png',
+    itemImageFront: 'public/images/home/drawings/drawing_hawk_heralds_gs_500px.png',
     itemInfoOne: itemInfoUniversals.drawingInfoOriginalDrawing,
     itemInfoTwo: itemInfoUniversals.drawingInfoPenAndInk,
     itemInfoThree: itemInfoUniversals.drawingInfoThreeAndQuarterInches,
     itemInfoFour: itemInfoUniversals.drawingInfoSignedOnBack,
     itemPrice: itemInfoUniversals.drawingPrice,
-    itemId: ''
+    itemId: 'DS7X4RVHNZXJS'
+  },
+  'drawingPinkMoon2020': {
+    itemClass: 'drawing-pink-moon-2020',
+    itemCategory: 'drawing',
+    itemName: 'Pink Moon 2020',
+    itemSubname: '',
+    itemImageFront: 'public/images/home/drawings/drawing_pink_moon_2020_gs_500px.png',
+    itemInfoOne: itemInfoUniversals.drawingInfoOriginalDrawing,
+    itemInfoTwo: itemInfoUniversals.drawingInfoPenAndInk,
+    itemInfoThree: itemInfoUniversals.drawingInfoThreeAndQuarterInches,
+    itemInfoFour: itemInfoUniversals.drawingInfoSignedOnBack,
+    itemPrice: itemInfoUniversals.drawingPrice,
+    itemId: 'WXGKHV6QX86BU'
+  },
+  'drawingRobinReveals': {
+    itemClass: 'drawing-robin-reveals',
+    itemCategory: 'drawing',
+    itemName: 'Robin Reveals',
+    itemSubname: '',
+    itemImageFront: 'public/images/home/drawings/drawing_robin_reveals_gs_500px.png',
+    itemInfoOne: itemInfoUniversals.drawingInfoOriginalDrawing,
+    itemInfoTwo: itemInfoUniversals.drawingInfoPenAndInk,
+    itemInfoThree: itemInfoUniversals.drawingInfoThreeAndQuarterInches,
+    itemInfoFour: itemInfoUniversals.drawingInfoSignedOnBack,
+    itemPrice: itemInfoUniversals.drawingPrice,
+    itemId: '27MQ7DNEX9DC2'
+  },
+  'drawingWormMoon2020': {
+    itemClass: 'drawing-worm-moon-2020',
+    itemCategory: 'drawing',
+    itemName: 'Worm Moon 2020',
+    itemSubname: '',
+    itemImageFront: 'public/images/home/drawings/drawing_worm_moon_2020_gs_500px.png',
+    itemInfoOne: itemInfoUniversals.drawingInfoOriginalDrawing,
+    itemInfoTwo: itemInfoUniversals.drawingInfoPenAndInk,
+    itemInfoThree: itemInfoUniversals.drawingInfoThreeAndQuarterInches,
+    itemInfoFour: itemInfoUniversals.drawingInfoSignedOnBack,
+    itemPrice: itemInfoUniversals.drawingPrice,
+    itemId: 'CCQ8NV9K78FNQ'
   },
   'lunarCalendarTwenty': {
     mainContent: item.itemPage,
--- assets/scripts/pages/item-page.js
+++ assets/scripts/pages/item-page.js
@@ -146,26 +146,26 @@ let itemPage = `
 `;
 
 const itemLoad = function (currentPage) {
-  let page = config.pagesInfo[currentPage]
+  let item = config.pagesInfo[currentPage]
   let htmlToFill = [
     // container-one content starts here
-    ['item-name', page.itemName],
-    ['item-subname', page.itemSubname],
-    ['item-info-one', page.itemInfoOne],
-    ['item-info-two', page.itemInfoTwo],
-    ['item-info-three', page.itemInfoThree],
-    ['item-info-four', page.itemInfoFour],
-    ['item-price', page.itemPrice],
+    ['item-name', item.itemName],
+    ['item-subname', item.itemSubname],
+    ['item-info-one', item.itemInfoOne],
+    ['item-info-two', item.itemInfoTwo],
+    ['item-info-three', item.itemInfoThree],
+    ['item-info-four', item.itemInfoFour],
+    ['item-price', item.itemPrice],
     // container-two content starts here
-    ['item-more-photo-pair-one', page.itemMorePhotoPairOne],
-    ['item-more-photo-pair-two', page.itemMorePhotoPairTwo],
-    ['item-more-photo-pair-three', page.itemMorePhotoPairThree],
-    ['item-more-photo-pair-four', page.itemMorePhotoPairFour],
-    ['item-more-info', page.itemMoreInfo]
+    ['item-more-photo-pair-one', item.itemMorePhotoPairOne],
+    ['item-more-photo-pair-two', item.itemMorePhotoPairTwo],
+    ['item-more-photo-pair-three', item.itemMorePhotoPairThree],
+    ['item-more-photo-pair-four', item.itemMorePhotoPairFour],
+    ['item-more-info', item.itemMoreInfo]
   ]
 
   // If the item has content for container-two then show container-two and load it
-  if (page.itemMoreInfo) {
+  if (item.itemMoreInfo) {
     document.querySelector('.container-two').classList.add('shown');
     document.querySelector('.container-two').innerHTML = containerTwoContent;
   }
@@ -178,10 +178,10 @@ const itemLoad = function (currentPage) {
   });
 
   // Set the main image's source, then add an item-specific category class (for item.scss breakpoints)
-  document.querySelector('.feature-image').src = page.itemImageFront;
-  document.querySelector('.feature-image').classList.add(page.itemCategory)
+  document.querySelector('.feature-image').src = item.itemImageFront;
+  document.querySelector('.feature-image').classList.add(item.itemCategory)
   // Set the form input's cart value
-  document.querySelector('.item-id').value = page.itemId;
+  document.querySelector('.item-id').value = item.itemId;
 
   // Also: Add custom functionality if a page is a lunar calendar item
   let itemTags = [
