f96144ac404c384399bdf2ae57ab0237fbece3c3
alecthibodeau <alecthibodeau@gmail.com>
2020-07-06 05:49:55 -0400

    Update home page text.

--- assets/scripts/pages/home.js
+++ assets/scripts/pages/home.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // import config from '../config.js';
-import store from '../store.js';
+// import store from '../store.js';
 
 // let drawingItem = `
 //   <div class="drawing-image">
@@ -29,20 +29,6 @@ import store from '../store.js';
 let homePage = `
   <div class="home-page">
     <div class="content">
-      <div class="content-top">
-        <a class="image-link" href="https://www.artistcommunities.org/arf" target="_blank">
-          <img src="public/images/home/artist_relief_fund_400px.png" />
-        </a>
-        <div class="announcement">
-          Through June 30th, 2020 I'm donating 50% of all sales at Huetown to the
-          <a class="text-link" href="https://www.artistcommunities.org/arf" target="_blank">Rhode Island Artist Relief Fund</a> &mdash; newly established in response to COVID-19.
-          I started collecting these contributions on April 11th, 2020 with my participation in
-          the Rhode Island School of Design's <a class="text-link" href="https://unbound.risd.edu" target="_blank">RISD Unbound 2020</a>. Much thanks to those who've already helped.
-          <div class="sig">
-            &ndash; Alec
-          </div>
-        </div>
-      </div>
 
       <div class="lunar-calendar-available">
         <div>
@@ -59,14 +45,14 @@ let homePage = `
   </div>
 `;
 
-const itemsToLoad = [
-  'drawingRobinReveals',
-  'drawingButternutBear',
-  'drawingHawkHeralds',
-  'drawingBisonBreathes',
-  'drawingPinkMoon2020',
-  'drawingWormMoon2020'
-];
+// const itemsToLoad = [
+//   'drawingRobinReveals',
+//   'drawingButternutBear',
+//   'drawingHawkHeralds',
+//   'drawingBisonBreathes',
+//   'drawingPinkMoon2020',
+//   'drawingWormMoon2020'
+// ];
 
 // const setCustomClassesAndContent = (elements, itemProperties) => {
 //   for (const element of elements) {
@@ -85,17 +71,17 @@ const itemsToLoad = [
 //   document.getElementById('drawingsItems').appendChild(itemElement);
 // };
 
-const itemCreate = (pageValue) => {
-  // for (const itemName of itemsToLoad) {
-  //   const item = config.itemsInfo[itemName];
-  //   const itemElement = document.createElement('div');
-  //   loadCreatedItem(itemElement);
-  //   setCustomClassesAndContent(store.getItemInfo(item, pageValue), item);
-  // }
-  return pageValue;
-};
+// const itemCreate = (pageValue) => {
+//   for (const itemName of itemsToLoad) {
+//     const item = config.itemsInfo[itemName];
+//     const itemElement = document.createElement('div');
+//     loadCreatedItem(itemElement);
+//     setCustomClassesAndContent(store.getItemInfo(item, pageValue), item);
+//   }
+//   return pageValue;
+// };
 
 export default {
   homePage,
-  itemCreate
+  // itemCreate
 };
